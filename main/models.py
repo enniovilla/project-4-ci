d4e0fe5d2bf3bc11683822b4fee610dd4c1144c3
Ennio Villa <enniosouzavilla@gmail.com>
2024-06-17 19:17:59 +0000

    Create discover spa model

--- main/models.py
+++ main/models.py
@@ -21,3 +21,17 @@ class Welcome(models.Model):
 
     def __str__(self):
         return self.heading
+
+
+class Discover(models.Model):
+    heading = models.CharField(max_length=100)
+    image = CloudinaryField('image')
+    paragraph1 = models.TextField(blank=True)
+    paragraph2 = models.TextField(blank=True)
+    paragraph3 = models.TextField(blank=True)
+    paragraph4 = models.TextField(blank=True)
+    paragraph5 = models.TextField(blank=True)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.heading
