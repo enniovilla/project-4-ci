29f0d9144a068796bce8255958660b7b4897dbc0
Ennio Villa <enniosouzavilla@gmail.com>
2024-06-17 08:59:23 +0000

    Change secret key

--- zen_oasis_spa/settings.py
+++ zen_oasis_spa/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+$o4j(1z3q1o43(_$d1z1iw4r4uhn%5e%f#^l1#)5rq0@!-nf2'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
