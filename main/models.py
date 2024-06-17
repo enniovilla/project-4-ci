b5c4cbd55b7fd58e5496480c8440b0f3744148a3
Ennio Villa <enniosouzavilla@gmail.com>
2024-06-17 10:43:34 +0000

    Add extra info to model

--- main/models.py
+++ main/models.py
@@ -2,4 +2,6 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 class CarouselImage(models.Model):
+    title = models.CharField(max_length=30, default='default title')
+    paragraph = models.CharField(max_length=100, default='default paragaph')
     image = CloudinaryField('image')
