02c75dccc6ae1f6ea555993a0e9bd68a2f6985ea
Ennio Villa <enniosouzavilla@gmail.com>
2024-06-17 12:19:20 +0000

    Change carousel model

--- main/models.py
+++ main/models.py
@@ -2,6 +2,13 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 class CarouselImage(models.Model):
-    title = models.CharField(max_length=30, default='default title')
-    paragraph = models.CharField(max_length=100, default='default paragaph')
+    title = models.CharField(max_length=50, default='default title')
+    description = models.CharField(max_length=100, default='default paragaph')
     image = CloudinaryField('image')
+    order = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        ordering = ['order']
+
+    def __str__(self):
+        return self.title
