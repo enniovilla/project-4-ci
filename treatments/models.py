f8c98f590b5318c1010532653beaca48e9c57ed6
Ennio Villa <enniosouzavilla@gmail.com>
2024-06-18 10:59:24 +0000

    Add duration and price

--- treatments/models.py
+++ treatments/models.py
@@ -14,6 +14,8 @@ class AccordionItem(models.Model):
     section = models.ForeignKey(Section, related_name='accordion_items', on_delete=models.CASCADE)
     header = models.CharField(max_length=100)
     body = models.TextField()
+    duration = models.CharField(max_length=50)
+    price = models.DecimalField(max_digits=10, decimal_places=2) 
 
     def __str__(self):
         return self.header
