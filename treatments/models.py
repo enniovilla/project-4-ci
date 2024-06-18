68cd925a815873a9a97616d3a4c5de5fa84db0dd
Ennio Villa <enniosouzavilla@gmail.com>
2024-06-18 10:11:51 +0000

    Create treatments models

--- treatments/models.py
+++ treatments/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Section(models.Model):
+    title = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.title
+
+
+class AccordionItem(models.Model):
+    section = models.ForeignKey(Section, related_name='accordion_items', on_delete=models.CASCADE)
+    header = models.CharField(max_length=100)
+    body = models.TextField()
+
