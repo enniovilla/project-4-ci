99db9800154e7d917f39bbf9067ffcc74b06e3aa
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-06-19 11:18:58 +0000

    Add contact view

---
+++ contact/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .forms import ContactForm
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ContactForm()
+
+    return render(request, 'contact/contact.html', {'form': form})
