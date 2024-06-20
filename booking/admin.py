6e7d48a1c98f0df1b9a3cdb56365331e32043406
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-06-20 21:25:36 +0000

    Add bookings to admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Booking
 
-# Register your models here.
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date_arrival', 'time_arrival', 'date_leave', 'time_leave')
+    list_display_links = ('user', 'date_arrival', 'time_arrival', 'date_leave', 'time_leave')
+    list_filter = ('user', 'date_arrival', 'time_arrival', 'date_leave', 'time_leave')
+    search_fields = ('user__username', 'treatment__header',)
+
+admin.site.register(Booking, BookingAdmin)
