e2f01a67d391995f903c1c6034768c2d2247917f
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-06-20 23:30:59 +0000

    Change booking form style

--- booking/forms.py
+++ booking/forms.py
@@ -13,10 +13,10 @@ class BookingForm(forms.ModelForm):
         fields = ['date_arrival', 'time_arrival', 'date_leave', 'time_leave']
         widgets = {
             'user': forms.HiddenInput,
-            'date_arrival': DateInput(),
-            'date_leave': DateInput(),
-            'time_arrival': TimeInput(),
-            'time_leave': TimeInput(),
+            'date_arrival': DateInput(attrs={'class': 'form-control fw-bold'}),
+            'date_leave': DateInput(attrs={'class': 'form-control fw-bold'}),
+            'time_arrival': TimeInput(attrs={'class': 'form-control fw-bold'}),
+            'time_leave': TimeInput(attrs={'class': 'form-control fw-bold'}),
         }
 
     def __init__(self, *args, **kwargs):
