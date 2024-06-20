e1057747088a47e978fd58cd86ea0fba0576a1fc
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-06-20 23:59:41 +0000

    Remove description

--- main/models.py
+++ main/models.py
@@ -3,7 +3,6 @@ from cloudinary.models import CloudinaryField
 
 class CarouselImage(models.Model):
     title = models.CharField(max_length=50)
-    description = models.CharField(max_length=100, blank=True)
     image = CloudinaryField('image')
     order = models.PositiveIntegerField(default=0)
 
