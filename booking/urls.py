a021672d2a41de01280145534b9ef353effe107b
Ennio Souza Villa <enniosouzavilla@gmail.com>
2024-06-20 23:13:14 +0000

    Add delete booking path

--- booking/urls.py
+++ booking/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('booking/', views.booking, name='booking'),
     path('my_bookings/', views.my_bookings, name='my_bookings'),
     path('edit_booking/<int:booking_id>/', views.edit_booking, name='edit_booking'),
+    path('delete_booking/<int:booking_id>/', views.delete_booking, name='delete_booking'),
 ]
